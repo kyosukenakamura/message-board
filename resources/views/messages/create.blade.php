4cc4740fc6afaca06d057151b2705dd3f7a24884
nakada.nakada <nakamurato830@gmail.com>
2017-11-06 15:37:26 +0000

    create syusei

--- resources/views/messages/create.blade.php
+++ resources/views/messages/create.blade.php
@@ -10,11 +10,11 @@
             {!! Form::model($message, ['route' => 'messages.store']) !!}
              
                  <div class="form-group">
-                    {!! Form::label('title', 'タイトル') !!}
+                    {!! Form::label('title', 'タイトル:') !!}
                     {!! Form::text('title', null, ['class' => 'form-control']) !!}
                 </div>
                 
-                <div class="form-control">
+                <div class="form-group">
                     {!! Form::label('content', 'メッセージ:') !!}
                     {!! Form::text('content', null, ['class' => 'form-control']) !!}
                 </div>
